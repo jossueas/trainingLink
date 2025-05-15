6d51994dd4cd0e0a804d64b0470d35d696454339
jossueas <jossueas@gmail.com>
2025-05-15 00:39:43 -0600

    Actualización de interfaz y lógica: - Home.aspx: ajustes en navegación - scripts.js: mejora de funciones JS generales - registroEntrenamiento: mejoras en UI y backend - trainingLink.csproj: referencia a nuevos archivos

---
+++ UI/maintenance/maintenanceAccess/access.aspx.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace trainingLink.UI.maintenance.maintenanceAccess
+{
+    public partial class access : Page
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["TrainingLinkConnection"].ConnectionString;
+        string connectionStringGeneralData = ConfigurationManager.ConnectionStrings["GeneralDataConnection"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+
+
+
+
+                string code1 = Session["Code1"]?.ToString();
+                var permisos = PermisoHelper.ObtenerMenuKeysParaUsuario(code1);
+
+                linkInicio.Visible = true;
+                linkAccesos.Visible = permisos.Contains("acceso");
+                linkRegistroEntrenamiento.Visible = permisos.Contains("registroEntrenamiento");
+                linkRol.Visible = permisos.Contains("rol");
+                linkBusinessUnit.Visible = permisos.Contains("businessUnit");
+                linkTurno.Visible = permisos.Contains("turno");
+                linkMuda.Visible = permisos.Contains("muda");
+                linkArea.Visible = permisos.Contains("area");
+                linkScrap.Visible = permisos.Contains("scrap");
+                linkOperacion.Visible = permisos.Contains("operacion");
+
+                // Comentados pero listos
+                // linkEntrenadores.Visible = permisos.Contains("entrenadores");
+                // linkEntrenamientos.Visible = permisos.Contains("entrenamientos");
+
+                linkSalir.Visible = true; // Siempre visible
+
+                CargarUsuariosModal(); // ddlCode1
+                CargarModulos();       // ddlMenuKey
+                CargarUsuarios();      // filtro principal
+                CargarPermisos();      // tabla
+            }
+        }
+
+        protected void ddlUsuarios_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CargarPermisos();
+        }
+
+        private void CargarModulos()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT MenuKey, NombreMostrar FROM Menu ORDER BY NombreMostrar", conn))
+                {
+                    ddlMenuKey.DataSource = cmd.ExecuteReader();
+                    ddlMenuKey.DataTextField = "NombreMostrar";
+                    ddlMenuKey.DataValueField = "MenuKey";
+                    ddlMenuKey.DataBind();
+                }
+            }
+
+            ddlMenuKey.Items.Insert(0, new ListItem("Seleccione un módulo", ""));
+        }
+
+
+        protected void btnGuardarPermiso_Click(object sender, EventArgs e)
+        {
+            string idPermiso = hdnIdPermiso.Value;
+            string code1 = ddlCode1.SelectedValue;
+            string menuKey = ddlMenuKey.SelectedValue;
+            bool puedeVer = chkPuedeVer.Checked;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                if (string.IsNullOrEmpty(idPermiso))
+                {
+                    // INSERTAR
+                    using (SqlCommand cmd = new SqlCommand("sp_InsertPermiso", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@Code1", code1);
+                        cmd.Parameters.AddWithValue("@MenuKey", menuKey);
+                        cmd.Parameters.AddWithValue("@PuedeVer", puedeVer);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                else
+                {
+                    // ACTUALIZAR
+                    using (SqlCommand cmd = new SqlCommand("sp_UpdatePermiso", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@IdPermiso", int.Parse(idPermiso));
+                        cmd.Parameters.AddWithValue("@PuedeVer", puedeVer);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+
+            // Refrescar la tabla y cerrar modal
+            CargarPermisos();
+
+            // Registrar JS para cerrar el modal visualmente
+            ScriptManager.RegisterStartupScript(this, GetType(), "cerrarModalPermiso", "cerrarModalPermiso();", true);
+        }
+
+
+        private void CargarUsuarios()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT Code1, FullName FROM GeneralData.dbo.[User] ORDER BY FullName", conn))
+                {
+                    ddlUsuarios.DataSource = cmd.ExecuteReader();
+                    ddlUsuarios.DataTextField = "FullName";
+                    ddlUsuarios.DataValueField = "Code1";
+                    ddlUsuarios.DataBind();
+                }
+            }
+
+            ddlUsuarios.Items.Insert(0, new System.Web.UI.WebControls.ListItem("Todos", ""));
+        }
+
+        private void CargarUsuariosModal()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionStringGeneralData))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT Code1, FullName FROM [User] ORDER BY FullName", conn))
+                {
+                    ddlCode1.DataSource = cmd.ExecuteReader();
+                    ddlCode1.DataTextField = "FullName"; // Lo que se ve
+                    ddlCode1.DataValueField = "Code1";   // Lo que se guarda
+                    ddlCode1.DataBind();
+                }
+            }
+
+            ddlCode1.Items.Insert(0, new ListItem("Seleccione un usuario", ""));
+        }
+
+
+        private void CargarPermisos()
+        {
+            string code1 = ddlUsuarios.SelectedValue;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("sp_GetPermisos", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Code1", string.IsNullOrEmpty(code1) ? (object)DBNull.Value : code1);
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    gvPermisos.DataSource = dt;
+                    gvPermisos.DataBind();
+                }
+            }
+        }
+    }
+}
---
+++ UI/master/PermisoHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+public static class PermisoHelper
+{
+    public static List<string> ObtenerMenuKeysParaUsuario(string code1)
+    {
+        var permisos = new List<string>();
+        string connectionString = ConfigurationManager.ConnectionStrings["TrainingLinkConnection"].ConnectionString;
+
+        using (SqlConnection conn = new SqlConnection(connectionString))
+        {
+            string query = "SELECT MenuKey FROM PermisoUsuario WHERE Code1 = @Code1 AND PuedeVer = 1";
+
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@Code1", code1);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    permisos.Add(reader["MenuKey"].ToString());
+                }
+            }
+        }
+
+        return permisos;
+    }
+}
